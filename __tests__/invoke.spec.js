adc3edd772cc228e2ba9555ef7126f2532d6102f
divya <hello@shortdiv.com>
2018-06-02 17:54:01 +0800

    test: passing test

--- __tests__/invoke.spec.js
+++ __tests__/invoke.spec.js
@@ -1,27 +1,16 @@
-jest.setTimeout(20000)
+jest.setTimeout(90000)
 
 const create = require('@vue/cli-test-utils/createTestProject')
 const path = require('path')
 const cwd = path.resolve(__dirname, '../testProject')
 
-async function createAndInstall (name) {
-  const project = await create(name, {
+test('should create vuex modules', async () => {
+  const project = await create('example-vuex-project', {
     plugins: {
-      '@vue/cli-plugin-babel': {},
-      'vue-cli-plugin-vuex-modules': {}
+      '@vue/cli-plugin-babel': {}
     }
   }, cwd)
-  // mock install
-  // const pkg = JSON.parse(await project.read('package.json'))
-  // pkg.devDependencies['vue-cli-plugin-vuex-modules'] = 'file:../../'
-  // await project.write('package.json', JSON.stringify(pkg, null, 2)).catch(err => {console.log(err)})
-  return project
-}
-
-describe('invoke', () => {
-  test('should create vuex modules', async () => {
-    const project = await createAndInstall(`example-vuex-project`)
-    //await project.run(`${require.resolve('@vue/cli/bin/vue')} invoke vue-cli-plugin-vuex-modules`)
-    expect(project.has('src/state/store.js')).toBe(true)
-  })
+  await project.run(`${require.resolve('@vue/cli/bin/vue')} add vue-cli-plugin-vuex-modules@file:../../`)
+  await project.run(`${require.resolve('@vue/cli/bin/vue')} invoke vue-cli-plugin-vuex-modules`)
+  expect(project.has('src/state/store.js')).toBe(true)
 })
